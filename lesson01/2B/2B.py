fa195c1a457e24a0e6c61b7ad49219a95bad7c75
Andrey Volkov <53947294+marksdreamwall@users.noreply.github.com>
2021-09-23 15:27:31 +0300

    task 2B completed
    added task 2B

---
+++ lesson01/2B/2B.py
@@ -0,0 +1,40 @@
+# data = [2, 0, 1, 1, 0, 1, 0, 2, 1, 2]
+data = list(map(int, input().split()))
+gap = 0
+
+for i in range(len(data)):
+    if data[i] == 1:
+        i_f, i_b = i+1, i-1
+        b_gap, f_gap = 0, 0
+        while i_b >= 0:
+            if data[i_b] == 2:
+                b_gap = (i - i_b)
+                break
+            else:
+                i_b -= 1
+        while i_f <= len(data) - 1:
+            if data[i_f] == 2:
+                f_gap = (i_f - i)
+                break
+            else:
+                i_f += 1
+
+        if b_gap != 0 and f_gap != 0:
+            new_gap = min(b_gap, f_gap)
+        elif b_gap == 0:
+            new_gap = f_gap
+        else:
+            new_gap = b_gap
+
+        if new_gap > gap:
+            gap = new_gap
+
+print(gap)
+
+
+
+
+# print(gap)
+
+
+
